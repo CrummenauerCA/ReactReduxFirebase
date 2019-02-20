c2dcc2d11894fea4cf337283ba375fe6f306791e
Cezar Augusto Crummenauer <crummenauerca@gmail.com>
2019-02-20 04:58:23 -0300

    Adding SignedInLinks into the navabar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SignedInLinks from './SignedInLinks';
+
 
 const Navbar = () => {
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to='/' className="container brand-logo">MarioPlan</Link>
+        <SignedInLinks />
       </div>
     </nav>
   );
